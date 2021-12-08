b2f653df6667d5c90fb81ecf3fc4aa9435cf045b
Dominik Ferber <dominik.ferber@gmail.com>
2021-12-08 08:39:24 +0200

    change footer link to github.com/happykit/flags

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ export default function Home(props) {
 
       <footer className={styles.footer}>
         <a
-          href="https://happykit.dev/solutions/flags?ref=flags-example"
+          href="https://github.com/happykit/flags?ref=flags-example"
           target="_blank"
           rel="noopener noreferrer"
         >
