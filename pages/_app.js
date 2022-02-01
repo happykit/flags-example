5b03ac865612596c8e2764f0a1a62cf18915ffa6
Dominik Ferber <dominik.ferber@gmail.com>
2022-02-01 11:55:11 +0200

    add preconnect

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import "../styles/globals.css";
 import "../flags.config";
+import * as React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Head>
+        {/*
+          Preconnecting improves load times by establishing the connection upfront
+          https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/preconnect
+        */}
+        <link rel="preconnect" href="https://happykit.dev" />
+      </Head>
+      <Component {...pageProps} />;
+    </React.Fragment>
+  );
 }
 
 export default MyApp;
