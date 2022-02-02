3aee6f0d7b03b30a3d3c237df8cb97972e58ad7e
Dominik Ferber <dominik.ferber@gmail.com>
2022-02-02 08:28:24 +0200

    preconnect to href from config

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "../flags.config";
 import * as React from "react";
 import Head from "next/head";
+import { config } from "@happykit/flags/config";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,7 +12,10 @@ function MyApp({ Component, pageProps }) {
           Preconnecting improves load times by establishing the connection upfront
           https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/preconnect
         */}
-        <link rel="preconnect" href="https://happykit.dev" />
+        <link
+          rel="preconnect"
+          href={[config.endpoint, config.envKey].join("/")}
+        />
       </Head>
       <Component {...pageProps} />;
     </React.Fragment>
